10294b18aad8bd1a436368ac1b3b7aedf123e018
Dmitry862 <rk3tt@mail.ru>
2024-10-18 10:05:17 +0300

    feat: add solution  ura!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    {
+        const init = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+        const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+        const hundred = 'hundred';
+        if (number === 0) {
+            return 'zero'
+        }
+        if (number < 20) {
+            return init[number]
+        }
+        if (number < 100) {
+            let numberOfTens = Math.floor(number / 10);
+            let remain = number % 10
+            return tens[numberOfTens] + (!remain ? '' : ' ' + init[remain])
+        }
+
+        if (number < 1000) {
+            //451 411 450
+            let hundr = Math.floor(number / 100); //4 
+            let remain100 = number % 100;
+            console.log(remain100)
+            return init[hundr] + ' ' + hundred + (remain100 ? ' ' + toReadable(remain100) : '')
+        }
+
+
+    }
 }
+
+
+
+
+
+
+
